eb8d8a821388b5469c767e0b98ab12ce95d252ce
mray69 <myreyai69@gmail.com>
2025-01-05 19:08:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from rasa.core.run import serve_application
 
 if __name__ == "__main__":
     serve_application(
-        model_directory="models",
+        model_directory="Newchatbot\rasa_backend\models",
         endpoints="endpoints.yml",
         cors="*",
         enable_api=True,
